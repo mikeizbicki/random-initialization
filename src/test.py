19d6b2c789cfef1288947015ddb8adcd0240a11a
Mike Izbicki <mike@izbicki.me>
2018-01-11 10:59:08 -0800

    fixed matplotlib to run without a display

--- src/test.py
+++ src/test.py
@@ -87,6 +87,9 @@ import tensorflow as tf
 tf.Session()
 import random
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from matplotlib.animation import FuncAnimation
