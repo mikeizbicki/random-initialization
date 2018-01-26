91bd9e2ee46aea9c59d18f6d786461ca57c7fef6
Mike Izbicki <mike@izbicki.me>
2018-01-26 11:49:33 -0800

    restored dropout to lenet

--- src/model/lenet.py
+++ src/model/lenet.py
@@ -5,6 +5,7 @@ def modify_parser(subparsers):
     subparser = subparsers.add_parser('lenet')
 
     subparser.add_argument('--loss',choices=['mse','xentropy'],default='xentropy')
+    subparser.add_argument('--dropout',type=interval(float),default=-0.5)
     subparser.add_argument('--l2',type=interval(float),default=1e-6)
     subparser.add_argument('--l1',type=interval(float),default=0.0)
 
@@ -16,7 +17,7 @@ def inference(x_,data,opts,is_training):
     end_points = {}
    
     num_classes=data.dimY
-    dropout_keep_prob=1.0 #0.5
+    dropout_keep_prob=args['dropout']
 
     init = tf.contrib.layers.xavier_initializer(
             uniform=True,
