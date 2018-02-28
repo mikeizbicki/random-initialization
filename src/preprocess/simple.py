be882c0f113e870822dc60acdeef3371362ad5f3
Mike Izbicki <mike@izbicki.me>
2018-02-28 14:43:01 -0800

    parallelism

--- src/preprocess/simple.py
+++ src/preprocess/simple.py
@@ -15,6 +15,9 @@ def modify_parser(subparsers):
     subparser_preprocess.add_argument('--pad_dim',type=interval(int),default=0)
     subparser_preprocess.add_argument('--pad_dim_numbad',type=interval(int),default=0)
 
+    subparser_preprocess.add_argument('--parallel',type=int,default=10)
+    subparser_preprocess.add_argument('--prefetch',type=int,default=1000)
+
 def preprocess_data(data,partitionargs):
     import tensorflow as tf
     import numpy as np
@@ -103,24 +106,29 @@ def preprocess_data(data,partitionargs):
             else:
                 return (x,y,id)
 
-        data.train = data.train.map(unit_norm)
-        data.train = data.train.map(label_corruption)
-        data.train = data.train.map(label_unshifted_percentile)
-        data.train = data.train.map(gaussian_X)
-        data.train = data.train.map(zero_Y)
-        data.train = data.train.map(max_Y)
-        data.train = data.train.map(pad_dim)
-        data.train = data.train.map(triangle)
-        #data.train = data.train.shuffle(partitionargs['preprocess']['batch_size']*20,seed=0)
-        #data.train = data.train.batch(partitionargs['preprocess']['batch_size'])
+        with tf.device('/cpu:0'):
+            p=partitionargs['preprocess']['parallel']
+            data.train = data.train.map(num_parallel_calls=p,map_func=unit_norm)
+            data.train = data.train.map(num_parallel_calls=p,map_func=label_corruption)
+            data.train = data.train.map(num_parallel_calls=p,map_func=label_unshifted_percentile)
+            data.train = data.train.map(num_parallel_calls=p,map_func=gaussian_X)
+            data.train = data.train.map(num_parallel_calls=p,map_func=zero_Y)
+            data.train = data.train.map(num_parallel_calls=p,map_func=max_Y)
+            data.train = data.train.map(num_parallel_calls=p,map_func=pad_dim)
+            data.train = data.train.map(num_parallel_calls=p,map_func=triangle)
+            #data.train = data.train.shuffle(partitionargs['preprocess']['batch_size']*20,seed=0)
+            #data.train = data.train.batch(partitionargs['preprocess']['batch_size'])
+            data.train = data.train.prefetch(partitionargs['preprocess']['prefetch'])
 
-        data.valid = data.valid.map(unit_norm)
-        data.valid = data.valid.map(pad_dim)
-        #data.valid = data.valid.batch(partitionargs['preprocess']['batch_size_test'])
+            data.valid = data.valid.map(num_parallel_calls=p,map_func=unit_norm)
+            data.valid = data.valid.map(num_parallel_calls=p,map_func=pad_dim)
+            #data.valid = data.valid.batch(partitionargs['preprocess']['batch_size_test'])
+            data.valid = data.valid.prefetch(partitionargs['preprocess']['prefetch'])
 
-        data.test = data.test.map(unit_norm)
-        data.test = data.test.map(pad_dim)
-        #data.test = data.test.batch(partitionargs['preprocess']['batch_size_test'])
+            data.test = data.test.map(num_parallel_calls=p,map_func=unit_norm)
+            data.test = data.test.map(num_parallel_calls=p,map_func=pad_dim)
+            #data.test = data.test.batch(partitionargs['preprocess']['batch_size_test'])
+            data.test = data.test.prefetch(partitionargs['preprocess']['prefetch'])
 
         if partitionargs['preprocess']['pad_dim']>0:
             data.dimX=[
